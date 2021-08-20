0f325d6c29930598e71aab543bff563f6ba9f28d
sofiacrf <sofia.rojasflo@gmail.com>
2021-08-20 17:10:40 -0500

    set the provider

---
+++ client/src/utils/Carcontext.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-reducers';
+
+// Open /utils/CarContext.js and import { Provider } from react-redux and store from ./store.
+import store from './store';
+
+export default function CarProvider(props) {
+  // Return the provider component provided by react-redux
+  return <Provider store={store} {...props} />;
+}
