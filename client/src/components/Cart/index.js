37b68820843743c437a1e27374d598bfb273ebcc
sofiacrf <sofia.rojasflo@gmail.com>
2021-08-20 20:42:26 -0500

    deleted comments

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -70,65 +70,6 @@ const Cart = () => {
       </div>
     );
   }
-
-  // const [state, dispatch] = useStoreContext();
-  // const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
-
-  // useEffect(() => {
-  //   if (data) {
-  //     stripePromise.then((res) => {
-  //       res.redirectToCheckout({ sessionId: data.checkout.session });
-  //     });
-  //   }
-  // }, [data]);
-
-  // useEffect(() => {
-  //   async function getCart() {
-  //     const cart = await idbPromise('cart', 'get');
-  //     dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-  //   }
-
-  //   if (!state.cart.length) {
-  //     getCart();
-  //   }
-  // }, [state.cart.length, dispatch]);
-
-  // function toggleCart() {
-  //   dispatch({ type: TOGGLE_CART });
-  // }
-
-  // function calculateTotal() {
-  //   let sum = 0;
-  //   state.cart.forEach((item) => {
-  //     sum += item.price * item.purchaseQuantity;
-  //   });
-  //   return sum.toFixed(2);
-  // }
-
-  // function submitCheckout() {
-  //   const productIds = [];
-
-  //   state.cart.forEach((item) => {
-  //     for (let i = 0; i < item.purchaseQuantity; i++) {
-  //       productIds.push(item._id);
-  //     }
-  //   });
-
-  //   getCheckout({
-  //     variables: { products: productIds },
-  //   });
-  // }
-
-  // if (!state.cartOpen) {
-  //   return (
-  //     <div className="cart-closed" onClick={toggleCart}>
-  //       <span role="img" aria-label="trash">
-  //         🛒
-  //       </span>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
